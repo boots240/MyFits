bcde37ba151f5c291aed0c8790a337f1263cf8ec
Ali <Alimomoo310@gmail.com>
2024-01-06 16:51:43 -0600

    files upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Main/*.{html,js}"],
+  theme: {
+    extend: 
+    {
+         fontFamily:
+         {
+          Poppins: " 'Poppins' , serif",
+         }
+    },
+  },
+  plugins: [],
+}
+
